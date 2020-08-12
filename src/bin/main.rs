abc0a2b50375c8d5f8e62d13cf105a850bcd7419
Yugo Ogura <jigenbakdaaan@gmail.com>
2020-08-13 00:27:54 +0900

    feat: show stage

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,6 +5,22 @@ fn main() {
     handle_display();
 }
 
+const STAGE_WIDTH: usize = 10;
+const STAGE_HEIGHT: usize = 20;
 fn handle_display() {
-    println!("Hello, handleDisplay!");
+    let block_empty = String::from("･");
+    let block_filled = String::from("■");
+    let mut stage: [[bool; STAGE_WIDTH - 1]; STAGE_HEIGHT - 1] =
+        [[false; STAGE_WIDTH - 1]; STAGE_HEIGHT - 1];
+
+    for line in &stage {
+        for i in line {
+            if *i {
+                print!("{}{}{}", block_filled, block_filled, block_filled);
+            } else {
+                print!(" {} ", block_empty);
+            }
+        }
+        println!("")
+    }
 }
