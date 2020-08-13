6c4d4cc445259760b8e9f4424e667aa8ceb216f0
Yugo Ogura <jigenbakdaaan@gmail.com>
2020-08-13 13:07:23 +0900

    feat: tetrimino struct

---
+++ src/tetrimino.rs
@@ -0,0 +1,57 @@
+pub struct Tetrimino {
+  w: usize,
+  h: usize,
+  block: Vec<Vec<bool>>,
+}
+
+impl Tetrimino {
+  pub fn i_tetrimino() -> Self {
+    Self {
+      w: 4,
+      h: 1,
+      block: vec![vec![true, true, true, true]],
+    }
+  }
+  pub fn o_tetrimino() -> Self {
+    Self {
+      w: 2,
+      h: 2,
+      block: vec![vec![true, true], vec![true, true]],
+    }
+  }
+  pub fn s_tetrimino() -> Self {
+    Self {
+      w: 3,
+      h: 3,
+      block: vec![vec![false, true, true], vec![true, true, false]],
+    }
+  }
+  pub fn z_tetrimino() -> Self {
+    Self {
+      w: 3,
+      h: 3,
+      block: vec![vec![true, true, false], vec![false, true, true]],
+    }
+  }
+  pub fn j_tetrimino() -> Self {
+    Self {
+      w: 3,
+      h: 3,
+      block: vec![vec![true, false, false], vec![true, true, true]],
+    }
+  }
+  pub fn l_tetrimino() -> Self {
+    Self {
+      w: 3,
+      h: 3,
+      block: vec![vec![false, false, true], vec![true, true, true]],
+    }
+  }
+  pub fn t_tetrimino() -> Self {
+    Self {
+      w: 3,
+      h: 3,
+      block: vec![vec![false, true, false], vec![true, true, true]],
+    }
+  }
+}
