25f23cbbefc7281a1612a827138d4b33121b0472
小倉優悟 <jigenbakdaaan@gmail.com>
2020-08-14 13:52:34 +0900

    fix: mino def

--- src/tetrimino.rs
+++ src/tetrimino.rs
@@ -5,6 +5,7 @@ pub struct Tetrimino {
 }
 
 impl Tetrimino {
+  /// ■■■■
   pub fn i_tetrimino() -> Self {
     Self {
       w: 4,
@@ -12,6 +13,8 @@ impl Tetrimino {
       block: vec![vec![true, true, true, true]],
     }
   }
+  /// ■■
+  /// ■■
   pub fn o_tetrimino() -> Self {
     Self {
       w: 2,
@@ -19,38 +22,48 @@ impl Tetrimino {
       block: vec![vec![true, true], vec![true, true]],
     }
   }
+  ///  ■■
+  /// ■■
   pub fn s_tetrimino() -> Self {
     Self {
       w: 3,
-      h: 3,
+      h: 2,
       block: vec![vec![false, true, true], vec![true, true, false]],
     }
   }
+  /// ■■
+  ///  ■■
   pub fn z_tetrimino() -> Self {
     Self {
       w: 3,
-      h: 3,
+      h: 2,
       block: vec![vec![true, true, false], vec![false, true, true]],
     }
   }
+  /// ■
+  /// ■■■
   pub fn j_tetrimino() -> Self {
     Self {
       w: 3,
-      h: 3,
+      h: 2,
       block: vec![vec![true, false, false], vec![true, true, true]],
     }
   }
+  ///   ■
+  /// ■■■
   pub fn l_tetrimino() -> Self {
     Self {
       w: 3,
-      h: 3,
+      h: 2,
       block: vec![vec![false, false, true], vec![true, true, true]],
     }
   }
+  ///  ■
+  /// ■■■
   pub fn t_tetrimino() -> Self {
     Self {
       w: 3,
-      h: 3,
+      h: 2,
       block: vec![vec![false, true, false], vec![true, true, true]],
     }
   }
